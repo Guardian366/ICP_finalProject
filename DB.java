f3415e96727644c0ff532d1de3468acda0132d6d
sharon-adelaide <34895957+sharon-adelaide@users.noreply.github.com>
2018-04-23 21:24:34 +0000

    Initial Program

---
+++ DB.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package icpfinalproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+ static final String DB_URL = "jdbc:mysql://localhost/ObservatoryData";
+
+ //  Database credentials
+ static final String USER = "root";
+ static final String PASS = "";
+ Connection con=null;
+    
+ public DB(){
+    try{
+			 Class.forName("com.mysql.jdbc.Driver");
+		
+		    //STEP 3: Open a connection
+		    System.out.println("Connecting to database...");
+		    con = DriverManager.getConnection(DB_URL,USER,PASS);
+                     System.out.println("Connected database successfully...");
+      
+		}catch(Exception e){System.out.println(e);}
+		
+	}
+}
